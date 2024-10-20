0221182f12fc0a207fc2fbecf53b4840ec44ce22
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-10-20 08:49:20 +0530

    Initial

---
+++ main.py
@@ -0,0 +1,33 @@
+import languagemodels as lm
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+#var = lm.config['instruct_model']
+#print(var)
+
+class PromptRequest(BaseModel):
+    prompt : str
+
+
+@app.post('/generate/')
+def generate_response(request : PromptRequest):
+
+    try:
+        res = lm.do(request.prompt)
+        return {"response", res}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
